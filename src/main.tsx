a30abdf6077104f73d83613ad99cf85b06cec4ce
KHATRI OM KUMAR <khatriom12062007@gmail.com>
2026-05-10 16:05:31 +0530

    backend

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
+import { useAuthStore } from './store/authStore'
+
+// Load existing Supabase session before first render
+useAuthStore.getState().loadSession();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
